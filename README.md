8c204482702c58e748c19a9140356ee318675cf5
erfii <erfan.granmayeh@gmail.com>
2022-07-21 11:37:09 +0430

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Todo List
+A Signup app powered by React, Formik, Yup, Bootstrap
+
+##### [Live Demo](https://signup.erfjs.com/) on Netlify
+
+<p align="center">
+  <a src="https://signup.erfjs.com/" target="_blank"><img src="./readme/example.png" width="100%" height="auto" /></a>
+</p>
+
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import signup from './assets/signup.svg';
+import { Signup } from './components/Signup';
+
+function App() {
+  return (
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-5">
+          <Signup />
+        </div>
+        <div className="col-md-7 my-auto">
+          <img className="img-fluid w-100" src={signup} alt="signup"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
